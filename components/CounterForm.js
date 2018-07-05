e6b860a5b186b407cb9f4abf5126a802fc996e01
Da Liu <dluoft@gmail.com>
2018-07-05 13:42:01 -0400

    [counter form] added form page

---
+++ components/CounterForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  form: {}
+});
+
+class CounterForm extends Component {
+  render() {
+    return (
+      <View>
+        <Text>CounterForm</Text>
+      </View>
+    );
+  }
+}
+
+export default CounterForm;
